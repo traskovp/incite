c3d0eb8a0d2955a294aad38db1bedec79a35dba1
Olegatar-sas <oleg151160@mail.ru>
2020-12-01 22:53:23 +0300

    Обратная связь

---
+++ sender.php
@@ -0,0 +1,49 @@
+<?php
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $company = $_POST['company'];
+    $message = $_POST['message'];
+ 
+
+    // формируем URL в переменной $queryUrl
+    $queryUrl = 'https://webincite.bitrix24.by/rest/14/iw4azplkop71t08s/crm.lead.add.json';
+    // формируем параметры для создания лида в переменной $queryData
+    $queryData = http_build_query(array(
+        'fields' => array(
+    'TITLE' => 'Заявка c сайта',
+    'NAME' => $name,
+    'EMAIL' => Array(
+           "n0" => Array(
+               "VALUE" => "$email",
+               "VALUE_TYPE" => "WORK",
+           ),
+       ),
+       'PHONE' => Array(
+           "n0" => Array(
+               "VALUE" => "$phone",
+               "VALUE_TYPE" => "WORK",
+           ),
+       ),
+    'COMPANY' => $company,
+    'MESSAGE' => $message,
+  ),
+        'params' => array("REGISTER_SONET_EVENT" => "Y")
+    ));
+    // обращаемся к Битрикс24 при помощи функции curl_exec
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $queryUrl,
+        CURLOPT_POSTFIELDS => $queryData,
+    ));
+    $result = curl_exec($curl);
+    curl_close($curl);
+    $result = json_decode($result, 1);
+    if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description'].
+    "<br/>";
+ 
+?>
